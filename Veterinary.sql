df29871fe0dd94798fe2680deb084c1dfb7b0502
by-febry <undestiny2627@gmail.com>
2023-11-08 10:27:42 +0800

    feat/create-table-animals

--- Veterinary.sql
+++ Veterinary.sql
@@ -9,3 +9,14 @@ CREATE TABLE owners(
 );
 
 
+CREATE TABLE animals(
+	animalid INT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	species VARCHAR(50) NOT NULL,
+	breed VARCHAR(50) NOT NULL,
+	dateofbirth DATE,
+	gender VARCHAR(10) NOT NULL,
+	color VARCHAR(50) NOT NULL,
+	ownerid INT,
+	foreign key (ownerid) references owners(ownerid)
+);
