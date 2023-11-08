3326ddc893139091757a07c97d0f1f2777902304
by-febry <undestiny2627@gmail.com>
2023-11-08 12:36:19 +0800

    Correction #2

--- Veterinary.sql
+++ Veterinary.sql
@@ -4,11 +4,10 @@ CREATE TABLE owners(
 	ownerid INT PRIMARY KEY,
 	ofirstname VARCHAR(50) NOT NULL,
 	olastname VARCHAR(50) NOT NULL,
-	address VARCHAR(15) NOT NULL,
+	address VARCHAR(100) NOT NULL,
 	email VARCHAR(100) NOT NULL,
-   	phone VARCHAR(50) NOT NULL
+	phone VARCHAR(50) NOT NULL
 );
-
 
 CREATE TABLE animals(
 	animalid INT PRIMARY KEY,
