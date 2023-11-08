b15183863fe528ed631087904825b54428f78d29
by-febry <undestiny2627@gmail.com>
2023-11-08 10:22:06 +0800

    feat/create-database-vertinary

--- Veterinary.sql
+++ Veterinary.sql
@@ -1,10 +1,4 @@
+CREATE DATABASE veterinary;
 
-CREATE TABLE owners(
-	ownerid INT PRIMARY KEY,
-	ofirstname VARCHAR(50) NOT NULL,
-	olastname VARCHAR(50) NOT NULL,
-	address VARCHAR(15) NOT NULL,
-	email VARCHAR(100) NOT NULL
-);
 
 
