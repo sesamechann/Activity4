a80506e21eaf763854998f678dd33bde8c845cfa
sesamechann <mikhaelajose@gmail.com>
2023-11-08 10:12:09 +0800

    error

--- Veterinary.sql
+++ Veterinary.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE veterinary;
+
+CREATE TABLE owners(
+	ownerid INT PRIMARY KEY,
+	ofirstname VARCHAR(50) NOT NULL,
+	olastname VARCHAR(50) NOT NULL,
+	address VARCHAR(15) NOT NULL,
+	email VARCHAR(100) NOT NULL
+);
+
